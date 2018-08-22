2697c9af61a4cd0829348e769cf46bdce4655730
Junlin Mo <junlinmo@junlins-mac.local>
2018-08-23 02:50:19 +0800

    Initial Commit

---
+++ Shopping List TestTests/Shopping_List_TestTests.swift
@@ -0,0 +1,36 @@
+//
+//  Shopping_List_TestTests.swift
+//  Shopping List TestTests
+//
+//  Created by Junlin Mo on 8/23/18.
+//  Copyright © 2018 Junlin Mo. All rights reserved.
+//
+
+import XCTest
+@testable import Shopping_List_Test
+
+class Shopping_List_TestTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
